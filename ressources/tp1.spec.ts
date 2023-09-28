73db2d80e82170ea832748f172142c9705b10ba5
tutanck <tutanck@gmail.com>
2023-09-28 13:24:54 +0200

    call 911

---
+++ ressources/tp1.spec.ts
@@ -0,0 +1,18 @@
+import {HelloWorld} from './tp1';
+
+describe('TP1', () => {
+
+    let helloWorld: HelloWorld;
+
+    beforeEach(() => {
+        helloWorld = new HelloWorld();
+    });
+
+    it('should say hello to Zenika', () => {
+        expect(helloWorld.sayHello('Zenika')).toBe('Hello Zenika!');
+    });
+
+    it('should say hello to World', () => {
+        expect(helloWorld.sayHello()).toBe('Hello World!');
+    });
+});
